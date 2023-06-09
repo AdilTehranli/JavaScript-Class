6c2883df03de00cebc8b6bded42ce65a061b7704
AdilTehranli <adiljt@code.edu.az>
2023-06-09 17:25:30 +0400

    Updated

--- script.js
+++ script.js
@@ -4,19 +4,21 @@ class Company{
         this.companyName=companyName,
         this.locationName=locationName
     }
-    employee(){
+    employee=[
+
         [],
         [],
         []
         
-    }
+    ]
+    
     table(){
 
     
         let body=document.getElementById('tbody');
         let tr='';
         const employeeArr=this.employee
-        for(let i =0;i<this.employee.length;i++){
+        for(let i =0;i<employeeArr[0].length;i++){
             tr+=`
             <tr>
             <th scope="row">${i+1}</th>
@@ -35,10 +37,10 @@ class Company{
        let position=document.getElementById('position').value;
        let salary=document.getElementById('salary').value;
        const employeeArr=this.employee;
-        employeeArr[0].push(name);
+        employeeArr[0].push(name)
         employeeArr[1].push(position);
         employeeArr[2].push(salary);
-        console.log(name.value);
+        console.log(employeeArr);
     }
 }
 
