92a64748dcbe6c1690309dcb6f0b2dcef45520f2
AdilTehranli <adiljt@code.edu.az>
2023-06-09 16:26:05 +0400

    Updated

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-// class User{
-//     constructor(name,surname,age){
-//         this.name=name,
-//         this.surname=surname,
-//         this.age=age
-//     }
-    
-// }
-// const adil=new User('adil','tehranli',19)
-
-// class Person extends User{
-//      constructor(name,surname,age,works){
-//         super(name,surname,age),
-//         this.works=works
-//      }
-// }
-// const kenan=new Person('kenan','qehramanov',19,'frontend-developer');
-// console.log(kenan);
 
 class Company{
     constructor(companyName,locationName){
@@ -29,29 +11,42 @@ class Company{
         
     }
     table(){
-        let name=document.getElementById('name');
-        let position=document.getElementById('position');
-        let salary=document.getElementById('salary');
+
     
         let body=document.getElementById('tbody');
         let tr='';
+        const employeeArr=this.employee
         for(let i =0;i<this.employee.length;i++){
             tr+=`
             <tr>
-            <th scope="row">${i}</th>
-            <td>${name}</td>
-            <td>${position}</td>
-            <td>${salary}</td>
+            <th scope="row">${i+1}</th>
+            <td>${employeeArr[0][i]}</td>
+            <td>${employeeArr[1][i]}</td>
+            <td>${employeeArr[2][i]}</td>
             </tr>           
             `
             
         }
         body.innerHTML=tr;
     }
-    Add(){
-        
+   addData(){
+
+       let name=document.getElementById('name').value;
+       let position=document.getElementById('position').value;
+       let salary=document.getElementById('salary').value;
+       const employeeArr=this.employee;
+        employeeArr[0].push(name);
+        employeeArr[1].push(position);
+        employeeArr[2].push(salary);
+        console.log(name.value);
     }
 }
+
+function Add(){
+  
+    ca.addData();
+}
+
 const ca=new Company('Code Academy','28 may')
 const lo=new Company('Code Academy','Nizami')
 
